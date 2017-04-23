42d90c21e6b503ed041fb8608ab0a8db537ec512
gustavolopess <gustavo.hlopess@gmail.com>
2017-04-22 22:02:54 -0300

    one-point crossover

--- chessboard.py
+++ chessboard.py
@@ -13,9 +13,8 @@ class Chessboard(list):
         super(Chessboard, self).__init__()
         self.optimization = optimization
         self.extend(self.__genotype__())
-        self._fitness = None
-        self.fitness()
-
+        self.fitness = None
+        self.evaluate_fitness()
 
     def __genotype__(self):
         """
@@ -30,12 +29,7 @@ class Chessboard(list):
             rep = rep.append(ch) if not self.optimization else rep | {ch}
         return rep
 
-    @property
-    def fitness(self):
-        return self._fitness
-
-    @fitness.setter
-    def fitness(self):
+    def evaluate_fitness(self):
         # columns clashes
         clashes = 0 if self.optimization else abs(self.length - len(np.unique(self)))
         # diagonal clashes
@@ -45,25 +39,47 @@ class Chessboard(list):
                     clashes += 1
         # fitness is the opposite value of clashes
         # therefore the fitness is inversely proportional to number of clashes
-        self._fitness = -clashes
+        self.fitness = -clashes
 
     def mutate(self):
-        pos1 = random.randint(0, self.length - 1) # position to be mutated
-        if not self.optimization:
+        pos1 = random.randint(0, self.length - 1)  # position to be mutated
+
+        def bit_flip(self):
             # mutation at bit string chromosome
-            shift = random.randint(0, int(self.length-1).bit_length())
+            shift = random.randint(0, int(self.length - 1).bit_length())
             if self[pos1] & (1 << shift):
                 self[pos1] &= ~(1 << shift)
             else:
                 self[pos1] |= (1 << shift)
-        else:
-            pos2 = pos1
+
+        def swap(self):
+            pos2 = random.randint(0, self.length - 1)
             while pos2 == pos1:
-                pos2 = random.randint(0, self.length - 1) # position to be swapped with pos1
+                pos2 = random.randint(0, self.length - 1)  # position to be swapped with pos1
             self[pos1], self[pos2] = self[pos2], self[pos1]
 
-        self.fitness() # re-evaluate fitness
+        if not self.optimization:
+            bit_flip(self)
+        else:
+            swap(self)
+
+        self.evaluate_fitness()  # re-evaluate fitness
 
 
+def crossover(gen1: Chessboard, gen2: Chessboard):
+    class CrossoverNotSupportedException(Exception):
+        pass
+
+    length = len(gen1)
+    if length != len(gen2):
+        raise CrossoverNotSupportedException('Genotypes have different lengths')
+
+    def one_point(gen1, gen2):
+        point = random.randint(0, length - 1)
+        child1 = gen1[:point] + gen2[point:]
+        child2 = gen2[:point] + gen1[point:]
+        return child1, child2
+
+    return one_point(gen1, gen2)
 
 
