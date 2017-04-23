faca0de6301c8a10b0d3e6d0f7f3739bdf51974e
gustavolopess <gustavo.hlopess@gmail.com>
2017-04-22 21:21:07 -0300

    mutation and fitness evaluation done

--- chessboard.py
+++ chessboard.py
@@ -1,32 +1,69 @@
-# import numpy as np
-from bitstring import BitArray
+import numpy as np
 import random
 
 
 class Chessboard(list):
-    def __init__(self, n_queens: int = 8):
+    def __init__(self, n_queens: int = 8, optimization=False):
         """
         :param n_queens: number of queens at chessboard
-        randomly generate the initial population and compute its fitness
+        :param optimization: False implies bit string representation will be used
+        randomly generate the genotype and compute its fitness
         """
         self.length = n_queens
         super(Chessboard, self).__init__()
-        self.extend(self.__genotype_bit_str__())
-        self.fitness = list([])*n_queens
+        self.optimization = optimization
+        self.extend(self.__genotype__())
+        self._fitness = None
+        self.fitness()
 
-    def __genotype_bit_str__(self):
-        rep = set({})
-        bits_len = len(bin(self.length)) - 2
+
+    def __genotype__(self):
+        """
+        generate the genotype randomly. 
+        if optimization is True, cases of two or more chromosomes 
+        standing at same row are avoided
+        :return: rep, genotype with n_queens chromosomes 
+        """
+        rep = list() if not self.optimization else set({})
         while len(rep) < self.length:
-            gen = random.randint(0, self.length - 1)
-            bits = BitArray(uint=gen, length=bits_len)
-            rep = rep | {bits.bin}
+            ch = random.randint(0, self.length - 1)
+            rep = rep.append(ch) if not self.optimization else rep | {ch}
         return rep
 
-    def __genotype_int_array__(self, n_queens: int):
-        pass
+    @property
+    def fitness(self):
+        return self._fitness
 
-    def evaluate_fitness(self):
-        pass
+    @fitness.setter
+    def fitness(self):
+        # columns clashes
+        clashes = 0 if self.optimization else abs(self.length - len(np.unique(self)))
+        # diagonal clashes
+        for i in range(self.length):
+            for j in range(self.length):
+                if i != j and abs(i-j) == abs(self[i] - self[j]):
+                    clashes += 1
+        # fitness is the opposite value of clashes
+        # therefore the fitness is inversely proportional to number of clashes
+        self._fitness = -clashes
+
+    def mutate(self):
+        pos1 = random.randint(0, self.length - 1) # position to be mutated
+        if not self.optimization:
+            # mutation at bit string chromosome
+            shift = random.randint(0, int(self.length-1).bit_length())
+            if self[pos1] & (1 << shift):
+                self[pos1] &= ~(1 << shift)
+            else:
+                self[pos1] |= (1 << shift)
+        else:
+            pos2 = pos1
+            while pos2 == pos1:
+                pos2 = random.randint(0, self.length - 1) # position to be swapped with pos1
+            self[pos1], self[pos2] = self[pos2], self[pos1]
+
+        self.fitness() # re-evaluate fitness
+
+
 
 
