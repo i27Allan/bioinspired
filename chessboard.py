342e2b05784391698c8b8e8220451a5c5f337153
gustavolopess <gustavo.hlopess@gmail.com>
2017-04-23 16:20:02 -0300

    crossover with bitstring is done

--- chessboard.py
+++ chessboard.py
@@ -4,14 +4,13 @@ import random
 
 
 class Chessboard(list):
-    def __init__(self, n_queens: int = 8, from_list=None, from_bits: str = '', repr_optimization=False):
+    def __init__(self, n_queens: int = 8, from_list=None, repr_optimization=False):
         """
         :param n_queens: number of queens at chessboard
+        :param from_list: to initialize the chessboard genotype from a existing list 
         :param repr_optimization: False implies bit string representation will be used
         randomly generate the genotype and compute its fitness
         """
-        # if from_list is None:
-        #     from_list = []
         self.repr_optimization = repr_optimization
         if from_list:
             self.length = len(from_list)
@@ -78,9 +77,10 @@ class Chessboard(list):
         self.evaluate_fitness()  # re-evaluate fitness
 
     def __to_bits__(self):
+        # print the genotype in a bit string format
         bits = ''
         for n in self:
-            bits += BitArray(uint=n, length=3).bin
+            bits += BitArray(uint=n, length=3).bin + ' '
         return bits
 
     def __str__(self):
@@ -96,6 +96,7 @@ def crossover(gen1: Chessboard, gen2: Chessboard):
 
     length = len(gen1)
     if length != len(gen2):
+        # only same length genotypes can be merged
         raise CrossoverNotSupportedException('Genotypes have different lengths')
 
     repr_optimization = gen1.repr_optimization or gen2.repr_optimization
@@ -103,6 +104,12 @@ def crossover(gen1: Chessboard, gen2: Chessboard):
     def __one_point__(gen1, gen2):
         c1, c2 = [], []
         if repr_optimization:
+            """
+            if the representation is through integers, the crossover process
+            is applied slicing the left part of gen1 and the right part of gen2 to
+            be the child1 and the left part of gen2 and the right part of gen1 to 
+            be the child2.
+            """
             point = random.randint(0, length - 1)
             c1 = gen1[:point]
             c2 = gen2[:point]
@@ -121,30 +128,49 @@ def crossover(gen1: Chessboard, gen2: Chessboard):
                 if len2 < length and gen2[i] not in c2:
                     c2.append(gen2[i])
         else:
-            point = random.randint(0, 3*length-1)
-            print(point)
-            block, offset = int(point/3), point % 3
+            """
+            if the representation is through bit string, the crossover process is
+            similar to the related above, but with one difference: the point-of-slice
+            is now calculated considering the whole bits of the representation.
+            If the point-of-slice falls on the 'middle' of a number, bitwise operations
+            are performed with aim to slice this number.
+            
+            e.g:
+                PARENT1 001 001 100 110 110 000 100 011  
+                PARENT2 000 010 100 101 010 110 111 101
+                
+                POINT-OF-SLICE: 16
+                
+                falls on the 0-1st bit of 5th block of n-bits (with 0-indexed positions).
+                Therefore, the slicing will be this way:
+                
+                CHILD1: 001 001 100 110 0\\10 110 111 101 (left of '\\' is from PARENT1 and the right is from PARENT2)
+                CHILD2: 000 010 100 101 1\\00 000 100 011 (left of '\\' is from PARENT2 and the right is from PARENT1)
+            """
+            point = random.randint(0, 3*length-1)  # gets the point-of-slice
+            block, offset = int(point/3), point % 3  # figures out which block of n-bits the slicing will occur
+            block -= 1  # adjusts to 0-indexed
             if offset == 0:
+                # if the point fell on division of two blocks, just concatenate the arrays
                 c1 = gen1[:block] + gen2[block:]
                 c2 = gen2[:block] + gen1[block:]
             else:
-                next_block = block+1
-                n1 = gen1[next_block] & (1 << (3-offset))
-                n2 = gen2[next_block] & ~(1 << (3-offset))
-                n_res = n1+n2
-                c1 = gen1[:block] + [n_res] + gen2[next_block:]
+                # if the point fell in a middle of an block, the slice will divide this number's bits
+                next_block = block+1  # the next is where the offset is positioned
+                bit_size = len(bin(length-1)) - 2  # gets the size of each block
+                shift_size = bit_size - offset
 
-                n1 = gen2[next_block] & (1 << (3 - offset))
-                n2 = gen1[next_block] & ~(1 << (3 - offset))
-                n_res = n1 + n2
-                c2 = gen2[:block] + [n_res] + gen1[next_block:]
+                n1 = (gen1[next_block] >> shift_size) << shift_size  # gets the 'shift_size' first bits of parent1
+                n2 = gen2[next_block] & ((1 << shift_size) - 1)  # gets the 'shift_size' last bits of parent2
+                n_res = n1 | n2  # concatenate them
+                c1 = gen1[:block] + [n_res] + gen2[next_block:]  # builds the child1
+
+                n1 = (gen2[next_block] >> shift_size) << shift_size  # gets the 'shift_size' first bits of parent2
+                n2 = gen1[next_block] & ((1 << shift_size) - 1)  # gets the 'shift_size' last bits of parent1
+                n_res = n1 | n2  # concatenate them
+                c2 = gen2[:block] + [n_res] + gen1[next_block:]  # builds the child2
 
         return c1, c2
-
-        # print('POINT', point)
-        # child1 = list(set(gen1[:point] + gen2[point:] + gen1[point:]))
-        # child2 = list(set(gen2[:point] + gen1[point:] + gen2[point:]))
-        # return child1, child2
 
     child1, child2 = __one_point__(gen1, gen2)
     return Chessboard(from_list=child1, repr_optimization=repr_optimization), Chessboard(from_list=child2, repr_optimization=repr_optimization)
@@ -159,7 +185,5 @@ if __name__ == '__main__':
     c1, c2 = crossover(p1, p2)
     print(c1, c1.fitness)
     print(c2, c2.fitness)
-
-    # print(c)
 
 
